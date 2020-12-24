9f156980b581cd75d71c301413accbd096e0da11
章佳义 <zhangjiayi.1998@bytedance.com>
2020-12-24 17:10:36 +0800

    rua

--- main.go
+++ main.go
@@ -1,27 +1,30 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"main/cc"
 	"os"
+	"regexp"
 	"strings"
 )
 
 func lastOf(s []int) int {
 	return s[len(s)-1]
 }
+type ByteArr []byte
 
-func main() {
+func (input ByteArr)formatJson() ByteArr {
 	isInJson:=false
 	bracketColorStack := make([]int,5)
 	indentLv := 0
 	indentStr := "  "
-	input,_:=ioutil.ReadAll(os.Stdin)
-	var sb strings.Builder
+
+	var sb bytes.Buffer
 
 	for i := range input {
-		if input[i] == '{' || input[i] == '['{
+		if input[i] == '{' || (input[i] == '[' && isInJson == true) {
 			indentLv++
 			if isInJson == false {
 				sb.WriteByte('\n')
@@ -32,9 +35,9 @@ func main() {
 			sb.WriteString(cc.Colorize(lastOf(bracketColorStack), string(input[i])))
 			sb.WriteString("\n"+strings.Repeat(indentStr, indentLv))
 
-		} else if input[i] == '}' || input[i] == ']'{
+		} else if input[i] == '}' || (input[i] == ']' && isInJson == true) {
 			indentLv--
-			if indentLv >= 0{
+			if indentLv >= 0 {
 				sb.WriteByte('\n')
 				sb.WriteString(strings.Repeat(indentStr, indentLv))
 				sb.WriteString(cc.Colorize(lastOf(bracketColorStack), string(input[i])))
@@ -59,6 +62,28 @@ func main() {
 			}
 		}
 	}
-	sb.WriteByte('\n')
-	fmt.Print(sb.String())
+	return sb.Bytes()
+}
+
+
+
+func (input ByteArr)highlightKeywords() ByteArr {
+	strIpt := string(input)
+	keywordsMatch:=[]string{"(?i)error", "(?i)warn", "(?i)info", "(?i)debug"}
+	colorList:=[]int{cc.Red, cc.Yellow, cc.Cyan, cc.Green}
+
+	for i:=range keywordsMatch {
+		r:=regexp.MustCompile(keywordsMatch[i])
+		strIpt = r.ReplaceAllStringFunc(strIpt, func(s string) string{
+			return cc.Colorize(colorList[i], s)
+		})
+	}
+	return []byte(strIpt)
+}
+
+func main() {
+	var input ByteArr
+	input,_=ioutil.ReadAll(os.Stdin)
+	res := input.highlightKeywords().formatJson()
+	fmt.Print(string(res))
 }
